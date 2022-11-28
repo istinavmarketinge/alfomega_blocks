07e891748c5eb19237790b0c926bf43843a9bcb3
Garapov Ruslan <ruslangarapov1994@gmail.com>
2022-11-28 12:29:23 +0300

    new forms and tables created

--- src/js/app.js
+++ src/js/app.js
@@ -15,6 +15,7 @@ var isSafari = navigator.vendor && navigator.vendor.indexOf('Apple') > -1 &&
 
 
 window.first_table = new Table('.table__js', '#table');
+window.second_table = new Table('.table__js', '#table2');
 window.first_form = new Form('#personalForm1');
 window.second_form = new Form('#personalForm2');
 
@@ -23,6 +24,7 @@ if (isSafari) {
 }
 
 $(document).ready(function() {
+    second_table.init();
     first_table.init();
     first_form.init();
     second_form.init();
